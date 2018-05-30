c446c06ab806b7e09906a32bf161216c9c3da06e
Madhuvanthi H <madhuvanthih@virtusa.com>
2018-05-30 18:41:18 +0530

    DOM

---
+++ app.js
@@ -0,0 +1,47 @@
+function identifyById() {
+    console.log(document.getElementById('para1'));
+}
+
+function identifyByClass() {
+    console.log(document.getElementsByClassName('class1'));
+}
+
+function identifyByTag() {
+    console.log(document.getElementsByTagName('p'));
+}
+
+function changeBackground() {
+   document.getElementById('block1').style.backgroundColor="orange";
+}
+
+function increaseFont() {
+document.getElementById("block2").style.fontSize="30px";
+}
+
+function changeFontColor() {
+    document.getElementById("block3").style.color = "green";
+}
+
+function revertColor() {
+    document.getElementById("block3").style.color = "black";
+}
+
+function hide() {
+    document.getElementById("block4").style.visibility = "hidden";
+}
+
+function changeBackColorForAllBox() {
+
+}
+
+
+function addAdjacent() {
+}
+
+function removePara() {
+
+}
+
+function animatePara() {
+
+}
